ba6c1a8cc5beaa0285912029a8d8aa88e5c27e77
gudtldn <gudtldn1234@gmail.com>
2025-03-13 20:11:41 +0900

    feat: FName 클래스 및 문자열 해싱 기능 추가

---
+++ Source/Core/UObject/NameTypes.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "Core/HAL/PlatformType.h"
+
+class FString;
+
+class FName
+{
+	// uint32 DisplayIndex;    // 원본 문자열의 Hash
+	uint32 ComparisonIndex; // 비교시 사용되는 Hash
+
+public:
+	FName() : ComparisonIndex(0) {}
+	FName(const WIDECHAR* Name);
+	FName(const ANSICHAR* Name);
+	FName(const FString& Name);
+
+	FString ToString() const;
+	// bool Equals(const FName& Other) const;
+
+	bool operator==(const FName& Other) const;
+};
