1bbc01a0636935999b043edf44c1085cf1d08dfc
gudtldn <gudtldn1234@gmail.com>
2025-03-14 14:43:01 +0900

    feat: 메모리 덤프 기능 추가

--- main.cpp
+++ main.cpp
@@ -1,10 +1,63 @@
 #pragma comment(lib, "user32")
 #pragma comment(lib, "d3d11")
 #pragma comment(lib, "d3dcompiler")
+#pragma comment(lib, "dbghelp.lib")
+
+#include "Core/HAL/PlatformType.h"
+#include <dbghelp.h>
 
 #include "Core/Engine.h"
 #include "Core/Config/ConfigManager.h"
 
+
+namespace
+{
+// 예외 처리기 및 메모리 덤프 생성
+LONG WINAPI ExceptionCallBack(EXCEPTION_POINTERS* exceptionInfo)
+{
+	MINIDUMP_EXCEPTION_INFORMATION info {
+		.ThreadId = GetCurrentThreadId(),
+		.ExceptionPointers = exceptionInfo,
+		.ClientPointers = false
+	};
+
+	const std::wstring stamp(L"crash.dmp");
+	const HANDLE hFile = CreateFileW(
+		stamp.c_str(),
+		GENERIC_WRITE,
+		FILE_SHARE_WRITE,
+		nullptr,
+		CREATE_ALWAYS,
+		FILE_ATTRIBUTE_NORMAL,
+		nullptr
+	);
+
+#ifdef _DEBUG
+	constexpr DWORD flags = MiniDumpWithFullMemory
+						| MiniDumpWithFullMemoryInfo
+						| MiniDumpWithHandleData
+						| MiniDumpWithUnloadedModules
+						| MiniDumpWithThreadInfo;
+#else
+	constexpr DWORD flags = MiniDumpNormal;
+#endif
+
+	// 위에서 받은 내용들을 토대로 덤프 파일을 만든다.
+	MiniDumpWriteDump(
+		GetCurrentProcess(),
+		GetCurrentProcessId(),
+		hFile,
+		static_cast<MINIDUMP_TYPE>(flags),
+		&info,
+		nullptr,
+		nullptr
+	);
+	CloseHandle(hFile);
+
+	MessageBoxW(nullptr, L"An error occurred. The program will be terminated.", L"Error", MB_OK | MB_ICONERROR);
+	return 0L;
+}
+}
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nShowCmd)
 {
@@ -12,6 +65,10 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
 	UNREFERENCED_PARAMETER(hPrevInstance);
 	UNREFERENCED_PARAMETER(lpCmdLine);
 	UNREFERENCED_PARAMETER(nShowCmd);
+
+	// 예외 처리기 등록
+	SetUnhandledExceptionFilter(ExceptionCallBack);
+
 
 	UConfigManager::Get().LoadConfig("editor.ini");
 
