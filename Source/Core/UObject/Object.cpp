4897482639d0be7b2bb3bfed047466cddef90b8f
gudtldn <gudtldn1234@gmail.com>
2025-03-15 19:13:42 +0900

    feat: UObject 클래스에 RTTI 지원을 위한 StaticClass 및 IsA 메서드 추가

--- Source/Core/UObject/Object.cpp
+++ Source/Core/UObject/Object.cpp
@@ -1,10 +1,28 @@
 ﻿#include "Core/UObject/Object.h"
+#include "Class.h"
 
-UObject::UObject()
+
+UClass* UObject::StaticClass()
 {
-
+	static std::unique_ptr<UClass, UClassDeleter> StaticClassInfo = nullptr;
+	if (!StaticClassInfo)
+	{
+		constexpr size_t ClassSize = sizeof(UClass);
+		void* RawMemory = FPlatformMemory::Malloc<EAT_Object>(ClassSize);
+		UClass* ClassPtr = new(RawMemory) UClass{"UObject", sizeof(UObject), alignof(UObject), nullptr};
+		StaticClassInfo = std::unique_ptr<UClass, UClassDeleter>(ClassPtr, UClassDeleter{});
+	}
+	return StaticClassInfo.get();
 }
 
-UObject::~UObject()
+UObject::UObject()
+	: NamePrivate("None")
+	, ClassPrivate(nullptr)
 {
+}
+
+bool UObject::IsA(const UClass* SomeBase) const
+{
+	const UClass* ThisClass = GetClass();
+	return ThisClass->IsChildOf(SomeBase);
 }
