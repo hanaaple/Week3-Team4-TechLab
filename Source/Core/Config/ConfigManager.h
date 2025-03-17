c76bdc06efbab2b7588ecece0b2f7ed33aeac4bb
gudtldn <gudtldn1234@gmail.com>
2025-03-17 18:11:10 +0900

    refactor: 주석 형식 통일 및 불필요한 코드 제거

--- Source/Core/Config/ConfigManager.h
+++ Source/Core/Config/ConfigManager.h
@@ -10,30 +10,20 @@ using ConfigData = TMap<std::string, Section>;
 class UConfigManager : public TSingleton<UConfigManager>
 {
 public:
-	/// <summary>
-	/// ini 파일을 읽어들입니다.
-	/// </summary>
+	/** ini 파일을 읽어들입니다. */
 	bool LoadConfig(const std::string& InConfigName);
 
-	/// <summary>
-	/// ini 파일을 저장합니다.
-	/// </summary>
+	/** ini 파일을 저장합니다. */
 	bool SaveConfig(const std::string& InConfigName);
 
-	/// <summary>
-	/// Section과 Key를 이용하여 값을 가져옵니다.
-	/// </summary>
+	/** Section과 Key를 이용하여 값을 가져옵니다. */
 	std::string GetValue(const std::string& InSection, const std::string& InKey);
 
-	/// <summary>
-	/// Section과 Key를 이용하여 Value을 설정합니다.
-	/// </summary>
+	/** Section과 Key를 이용하여 Value을 설정합니다. */
 	void SetValue(const std::string& InSection, const std::string& InKey, const std::string& InValue);
 
 private:
-	/// <summary>
-	/// 문자열 앞뒤의 공백 제거 함수
-	/// </summary>
+	/** 문자열 앞뒤의 공백 제거 함수 */
 	std::string trim(const std::string& InStr) const;
 
 private:
